1d5c3622bb033f206a4708cb7da4afcc3c296e3b
Mattia Lecci <mattia.lecci@gmail.com>
2020-07-01 11:46:32 +0200

    Update AdAmcPacket env with retx
    Updated agents and main to account for that

--- wireless/agents/rate_manager_agents.py
+++ wireless/agents/rate_manager_agents.py
@@ -95,7 +95,7 @@ class ArfAgent:
             "mcs" : int
                 MCS used for the previous packet(s)
             "pkt_succ" : int
-                Last packet(s) were successful (1) or not (0). If None, the communication just started.
+                Last packet(s) were successful (1) or not. If None, the communication just started.
         info : dict
             Not used, kept to maintain the same signature for all agents.
 
@@ -110,12 +110,8 @@ class ArfAgent:
 
         mcs = state["mcs"][0]
 
-        if success == 0:
-            # If transmission fails: fall back
-            self._pkt_succ_count = 0
-            return max(0, mcs - 1)
-
-        else:
+        if success == 1:
+            # If transmission is successful
             self._pkt_succ_count += 1
 
             if self._pkt_succ_count == 10:
@@ -126,6 +122,11 @@ class ArfAgent:
 
             else:
                 return mcs
+
+        else:
+            # If transmission fails: fall back
+            self._pkt_succ_count = 0
+            return max(0, mcs - 1)
 
 
 class AarfAgent:
@@ -172,19 +173,8 @@ class AarfAgent:
 
         mcs = state["mcs"][0]
 
-        if success == 0:
-            # If transmission fails
-            self._pkt_succ_count = 0
-            self._pkt_fail_count += 1
-
-            if self._pkt_fail_count == 1:
-                self._succ_to_advance = min(50, 2 * self._succ_to_advance)
-            else:
-                self._succ_to_advance = 10
-
-            return max(0, mcs - 1)
-
-        else:
+        if success == 1:
+            # If transmission is successful
             self._pkt_succ_count += 1
             self._pkt_fail_count = 0
 
@@ -196,6 +186,18 @@ class AarfAgent:
 
             else:
                 return mcs
+
+        else:
+            # If transmission fails
+            self._pkt_succ_count = 0
+            self._pkt_fail_count += 1
+
+            if self._pkt_fail_count == 1:
+                self._succ_to_advance = min(50, 2 * self._succ_to_advance)
+            else:
+                self._succ_to_advance = 10
+
+            return max(0, mcs - 1)
 
 
 class OnoeAgent:
@@ -211,7 +213,7 @@ class OnoeAgent:
     def __init__(self, action_space, theta_r=0.5, theta_c=0.1, n_c=10, window=100e-3):
         self._max_mcs = action_space.n - 1
 
-        self. _theta_r = theta_r
+        self._theta_r = theta_r
         self._theta_c = theta_c
         self._n_c = n_c
         self._window = window
@@ -288,4 +290,3 @@ class OnoeAgent:
             self._pkt_succ_count += 1
         else:
             self._pkt_fail_count += 1
-
--- wireless/scripts/launch_ad_amc_packet.py
+++ wireless/scripts/launch_ad_amc_packet.py
@@ -7,75 +7,104 @@ import matplotlib.pyplot as plt
 from wireless.agents import rate_manager_agents
 
 # Env params
-CAMPAIGN = "scenarios_v1"
+CAMPAIGN = "scenarios_v3"
 SCENARIOS_LIST = ["Journal1Lroom_1.csv"]  # List of scenarios for the environment
 HISTORY_LENGTH = 1  # The number of past SNR values to consider for the state
 NET_TIMESTEP = 5e-3  # The real network timestep [s]
 OBS_DURATION = None  # Observation duration [s]; If None, the whole scenario is the observation
-
-# Agents params
-AGENT_TYPE = "ONOE"
+HARQ_RETX = 2  # The number of HARQ retransmission after the first transmission
 
 
-def main():
+def main(agent_type="ARF"):
     assert len(SCENARIOS_LIST) == 1, "Exactly 1 scenario should be evaluated with this script"
 
     env = gym.make("AdAmcPacket-v0", campaign=CAMPAIGN, scenarios_list=SCENARIOS_LIST, obs_duration=OBS_DURATION,
-                   history_length=HISTORY_LENGTH, net_timestep=NET_TIMESTEP)
+                   history_length=HISTORY_LENGTH, net_timestep=NET_TIMESTEP, harq_retx=HARQ_RETX)
 
-    if AGENT_TYPE == "ARF":
+    if agent_type == "ARF":
         agent = rate_manager_agents.ArfAgent(env.action_space)
-    elif AGENT_TYPE == "AARF":
+    elif agent_type == "AARF":
         agent = rate_manager_agents.AarfAgent(env.action_space)
-    elif AGENT_TYPE == "ONOE":
+    elif agent_type == "ONOE":
         agent = rate_manager_agents.OnoeAgent(env.action_space)
     else:
-        raise ValueError(f"Agent '{AGENT_TYPE}' not recognized")
+        raise ValueError(f"Agent '{agent_type}' not recognized")
 
     # Init stats
-    reward_t = []
-    time = []
     done = False
-    tot_pkts = 0
-    tot_mb_generated = 0
+    tot_sent_pkts = 0  # Excluding retransmissions
+    tot_mb_generated = 0  # Excluding retransmissions
+    tot_mb_rx = 0
+    tot_pkts = 0  # Including retransmissions
+    tot_rx_pkts = 0
+
+    rx_pkts_retx = []
+    rx_pkts_delay = []
+
+    time = []
     mcs_t = []
+    reward_t = []
+    retx_t = []
+    delay_t = []
 
     # Run agent
     state = env.reset()
     info = {}
 
     while not done:
-        if AGENT_TYPE == "ARF":
+        if agent_type == "ARF":
             action = agent.act(state, info)
-        elif AGENT_TYPE == "AARF":
+        elif agent_type == "AARF":
             action = agent.act(state, info)
-        elif AGENT_TYPE == "ONOE":
+        elif agent_type == "ONOE":
             action = agent.act(state, info)
         else:
-            raise ValueError(f"Agent '{AGENT_TYPE}' not recognized")
+            raise ValueError(f"Agent '{agent_type}' not recognized")
 
         state, reward, done, info = env.step(action)
 
-        tot_mb_generated += info["pkt_size"] / 1e6
-        reward_t.append(reward)
-        time.append(info["current_time"])
-        mcs_t.append(state["mcs"][0])
+        if state["pkt_retx"][0] == 0:
+            # If retx == 0: new packet, irrespective of its success
+            tot_sent_pkts += 1
+            tot_mb_generated += info["pkt_size"] / 1e6
 
         tot_pkts += 1
 
-        env.close()
+        if state["pkt_succ"][0] == 1:
+            tot_rx_pkts += 1
+            tot_mb_rx += info["pkt_size"] / 1e6
+            rx_pkts_retx.append(state["pkt_retx"][0])
+            rx_pkts_delay.append(state["pkt_delay"][0])
+
+        reward_t.append(reward)
+        time.append(info["current_time"])
+        mcs_t.append(state["mcs"][0])
+        retx_t.append(state["pkt_retx"][0])
+        delay_t.append(state["pkt_delay"][0])
+
+    env.close()
 
     # Stats
-    avg_data_rate_mbps = tot_mb_generated / env.scenario_duration
-    avg_thr = np.sum(reward_t, dtype=np.int64) / env.scenario_duration
-    print(f"Avg data rate [Mbps]: {avg_data_rate_mbps}")
-    print(f"Avg throughput [Mbps]: {avg_thr / 1e6}")
-    print(f"Average reward: {np.mean(reward_t)}")
-    print(f"Total packets: {tot_pkts}")
+    avg_generated_rate_mbps = tot_mb_generated / env.scenario_duration
+    avg_thr_mbps = tot_mb_rx / env.scenario_duration
+    avg_reward = np.mean(reward_t)
+    psr = tot_rx_pkts / tot_sent_pkts
+    avg_succ_pkt_retx = np.mean(rx_pkts_retx)
+    avg_succ_pkt_delay = np.mean(rx_pkts_delay)
+
+    print("***")
+    print(f"Agent type: {agent_type}")
+    print(f"Avg generated data rate [Mbps]: {avg_generated_rate_mbps}")
+    print(f"Avg throughput [Mbps]: {avg_thr_mbps}")
+    print(f"Average reward: {avg_reward}")
+    print(f"Total packets sent: {tot_sent_pkts}, transmitted (including RETXs): {tot_pkts}")
+    print(f"Average packet success rate: {psr}")
+    print(f"Average successful packet RETX: {avg_succ_pkt_retx}")
+    print(f"Average successful packet delay [us]: {avg_succ_pkt_delay * 1e6}")
 
     # Plot results
     plt.figure()
-    plt.plot(time, mcs_t, label=AGENT_TYPE)
+    plt.plot(time, mcs_t, label=agent_type)
     plt.title(SCENARIOS_LIST[0])
     plt.ylabel("MCS [idx]")
     plt.xlabel("Time [s]")
@@ -83,6 +112,37 @@ def main():
     plt.grid()
     plt.show()
 
+    plt.figure()
+    plt.plot(time, retx_t, label=agent_type)
+    plt.title(SCENARIOS_LIST[0])
+    plt.ylabel("Retransmissions")
+    plt.xlabel("Time [s]")
+    plt.legend(loc="lower left")
+    plt.grid()
+    plt.show()
+
+    plt.figure()
+    plt.plot(time, [d * 1e3 for d in delay_t], label=agent_type)
+    plt.title(SCENARIOS_LIST[0])
+    plt.ylabel("Delay [ms]")
+    plt.xlabel("Time [s]")
+    plt.legend(loc="lower left")
+    plt.grid()
+    plt.show()
+
+    plt.figure()
+    plt.plot(time, np.cumsum(reward_t, dtype=np.float) / np.arange(1, len(reward_t) + 1), label=agent_type)
+    plt.title(SCENARIOS_LIST[0])
+    plt.ylabel("Average Cumulative Reward")
+    plt.xlabel("Time [s]")
+    plt.legend(loc="lower left")
+    plt.grid()
+    plt.show()
+
+    return
+
 
 if __name__ == '__main__':
-    main()
+    main(agent_type="ARF")
+    main(agent_type="AARF")
+    main(agent_type="ONOE")
